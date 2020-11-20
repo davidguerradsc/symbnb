bf18c3d798a92488c080e574e20993b02e6064bd
David Guerra Da Silva Cravo <david.guerradsc@gmail.com>
2020-11-20 22:22:14 +0100

    Logout ok
    Add logout function

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -9,10 +9,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccountController extends AbstractController
 {
     /**
+     * Permet d'afficher et de gérer le formulaire de connexion
+     * 
      * @Route("/login", name="account_login")
+     * @return Response
      */
-    public function login(): Response
+    public function login()
     {
         return $this->render('account/login.html.twig');
+    }
+
+    /**
+     * Permet de se déconnecter
+     * 
+     * @Route("/logout", name="account_logout")
+     *
+     * @return void
+     */
+    public function logout() {
+
     }
 }
